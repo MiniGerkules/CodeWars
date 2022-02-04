8f29485d82a340070009a8570c8b362408115624
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-02-04 19:44:52 +0300

    Find the uniq number in the list

---
+++ Python/FindUniq.py
@@ -0,0 +1,27 @@
+def find_uniq(arr):
+    uniq = {}
+    
+    for num in arr:
+        uniq.setdefault(num, 0)
+        uniq[num] += 1
+    
+    return min(uniq, key=uniq.get)
+
+
+def main():
+    results = []
+    mustBe = [2, 0.55, 10, 7]
+
+    results.append(find_uniq([1, 1, 1, 2, 1, 1]))
+    results.append(find_uniq([0, 0, 0.55, 0, 0]))
+    results.append(find_uniq([3, 10, 3, 3, 3]))
+    results.append(find_uniq([3, 3, 3, 3, 7]))
+
+    print(results, mustBe, sep='\n')
+
+    for res, must in zip(results, mustBe):
+        assert(res == must)
+
+
+if __name__ == '__main__':
+    main()
