ec9fa11315962ebd1458042d5d7e9e5e0d2c22a3
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-02-05 19:29:45 +0300

    Delete junk files and rename Main.java

--- Java/PlayingWithDigits.java
+++ Java/PlayingWithDigits.java
@@ -1,4 +1,14 @@
-public class DigPow {
+// https://www.codewars.com/kata/5552101f47fc5178b1000050
+
+class Main {
+    public static void main(String[] agrs) {
+        assert(1 == DigPow.digPow(89, 1));
+        assert(-1 == DigPow.digPow(92, 1));
+        assert(51 == DigPow.digPow(46288, 3));
+    }
+}
+
+class DigPow {
     public static long digPow(int n, int p) {
         String nInStr = String.valueOf(n);
         long resNum = 0;
--- Java/PlayingWithDigits/Main.java
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/5552101f47fc5178b1000050
-
-class Main {
-    public static void main(String[] agrs) {
-        assert(1 == DigPow.digPow(89, 1));
-        assert(-1 == DigPow.digPow(92, 1));
-        assert(51 == DigPow.digPow(46288, 3));
-    }
-}
