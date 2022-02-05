76ae1cf59c7f296c147ff280556b9e55d561309f
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-02-05 18:52:31 +0300

    Add kata links

--- C++/UniqueInOrder.cpp
+++ C++/UniqueInOrder.cpp
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/54e6533c92449cc251001667
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
--- Python/ToCamelCase.py
+++ Python/ToCamelCase.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/517abf86da9663f1d2000003
+
 import re
 
 def to_camel_case(text: str):
